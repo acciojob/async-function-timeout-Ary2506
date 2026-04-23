edad6a48c667535c209c72ac6c1faa016c54a5e4
Ary2506 <ap1895284@gmail.com>
2026-04-23 18:20:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+
+function wait(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+btn.addEventListener("click", async function () {
+  const text = document.getElementById("text").value;
+  const delay = parseInt(document.getElementById("delay").value);
+  const output = document.getElementById("output");
+
+  output.textContent = "";
+
+  await wait(delay);
+
+  output.textContent = text;
+});
